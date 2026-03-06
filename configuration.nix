b4cd602faecaf1814ea055baf77ba6c69d20b3da
rhannan1234 <ruairchannan@gmail.com>
2026-03-06 22:39:06 +0000

    Refactor config: move gaming tools to module, add user groups for LACT

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,6 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    # FIXED: Combined into a single string with proper quotes
     options = "--delete-older-than 7d --max-n-generations 10";
   };
 
@@ -81,22 +80,14 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.ruairc = {
     isNormalUser = true;
     description = "ruairc";
-    extraGroups = [ "networkmanager" "wheel" ];
+    # ADDED: video, render, and input groups for GPU control (LACT) and Gaming
+    extraGroups = [ "networkmanager" "wheel" "video" "render" "input" ];
     packages = with pkgs; [
     #  thunderbird
     ];
@@ -108,8 +99,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile. 
+  # Note: Gaming tools (Steam, LACT, MangoHud) are now managed in gaming.nix
   environment.systemPackages = with pkgs; [
     vim
     git
@@ -117,38 +108,13 @@
     curl
     yazi
     tree
-    lact
-    mangohud
-    goverlay
+    # Removed lact, mangohud, goverlay from here to avoid duplicates with gaming.nix
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html  ).
+  # Enable Ollama (defined in ollama.nix)
   services.my-ollama.enable = true;
-  services.lact.enable = true;
   
+  # Note: services.lact.enable is now handled in gaming.nix
+
   system.stateVersion = "25.11"; # Did you read the comment?
 }
