467b974bfdd1ebd55232e3d69755e187d560292f
rhannan1234 <ruairchannan@gmail.com>
2026-03-07 00:11:29 +0000

    FIX: Explicitly add nixpkgs-old to configuration.nix arguments

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+{ config, pkgs, nixpkgs-old, ... }:
 # your system. Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running 'nixos-help').
 
