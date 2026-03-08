26838913c54a389b9d89afba33ceb4d146b620ba
rhannan1234 <ruairchannan@gmail.com>
2026-03-08 23:27:20 +0000

    x

--- modules/multimedia.nix
+++ modules/multimedia.nix
@@ -1,12 +1,9 @@
 { config, pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    
-    # Replace discord with vesktop
-    # Vesktop includes Vencord by default in nixpkgs
     vesktop
-
-    # Latest official Spotify
     spotify
+    brave
+    obsidian
   ];
 
   nixpkgs.config.allowUnfree = true;
